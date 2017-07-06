c9ce95967423e44bc89bf475250da65af5d14c83
cades kao <cadeskao@gmail.com>
2017-07-06 17:10:40 +0800

    extract DefinitionPanel.js

---
+++ src/DefinitionPanel.js
@@ -0,0 +1,52 @@
+const panelBase = {
+  show() {
+    this.$el.style.display = 'block';
+  },
+  hide() {
+    this.$el.style.display = 'none';
+  },
+  clear() {
+    this.$el.innerHTML = '';
+  },
+  resetScroll() {
+    this.$el.scrollTop = 0;
+  },
+  updateDefinition(def) {
+    const content = `
+    <h2 style="font-size: 22px; font-weight: normal">${def.word}</h2>
+    <h3 style="font-size: 18px; font-weight: normal">${def.header}</h3>
+    <ol style="list-style-type: decimal">
+      ${def.contents.map((text) => {
+          return '<li style="font-size: 14px">' + text + '</li>'
+        }).join('')
+      }
+    </ol>
+  `;
+    this.$el.innerHTML = content;
+  },
+  getDOMNode() {
+    return this.$el;
+  }
+}
+
+function createDefinitionPanel() {
+  const $el = document.createElement('div');
+
+  Object.assign($el.style, {
+    position: 'fixed',
+    display: 'none',
+    right: '10px',
+    bottom: '10px',
+    width: `${window.innerWidth / 2}px`,
+    height: `${window.innerHeight / 2}px`,
+    backgroundColor: '#EFEFEF',
+    border: 'solid 1px #FFCB51',
+    overflowY: 'scroll',
+    paddingLeft: '15px',
+    paddingRight: '15px',
+  });
+
+  return Object.assign(Object.create(panelBase), {$el});
+}
+
+export {panelBase, createDefinitionPanel};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import getSelectedText from './getSelectedText';
+import {createDefinitionPanel} from './DefinitionPanel';
+
+const panel = createDefinitionPanel();
+var currentWord = '';
 
 document.body.addEventListener('mouseup', updateDefinitionPanelStatus);
 document.body.addEventListener('mousedown', updateDefinitionPanelStatus);
@@ -21,57 +25,6 @@ function updateDefinitionPanelStatus(e) {
     .then(() => panel.resetScroll()).then(() => panel.show());
 }
 
-const panelBase = {
-  show() {
-    this.$el.style.display = 'block';
-  },
-  hide() {
-    this.$el.style.display = 'none';
-  },
-  clear() {
-    this.$el.innerHTML = '';
-  },
-  resetScroll() {
-    this.$el.scrollTop = 0;
-  },
-  updateDefinition(def) {
-    const content = `
-    <h2 style="font-size: 22px; font-weight: normal">${def.word}</h2>
-    <h3 style="font-size: 18px; font-weight: normal">${def.header}</h3>
-    <ol style="list-style-type: decimal">
-      ${def.contents.map((text) => {
-          return '<li style="font-size: 14px">' + text + '</li>'
-        }).join('')
-      }
-    </ol>
-  `;
-    this.$el.innerHTML = content;
-  },
-  getDOMNode() {
-    return this.$el;
-  }
-}
-
-function createDefinitionPanel() {
-  const $el = document.createElement('div');
-
-  Object.assign($el.style, {
-    position: 'fixed',
-    display: 'none',
-    right: '10px',
-    bottom: '10px',
-    width: `${window.innerWidth / 2}px`,
-    height: `${window.innerHeight / 2}px`,
-    backgroundColor: '#EFEFEF',
-    border: 'solid 1px #FFCB51',
-    overflowY: 'scroll',
-    paddingLeft: '15px',
-    paddingRight: '15px',
-  });
-
-  return Object.assign(Object.create(panelBase), {$el});
-}
-
 function getDefinitionOf(word) {
   return fetch(`http://www.dictionary.com/browse/${word}`)
     .then((resp) => resp.text())
@@ -86,8 +39,5 @@ function getDefinitionOf(word) {
       return { word, header, contents };
     })
 }
-
-const panel = createDefinitionPanel();
-var currentWord = '';
 
 document.body.appendChild(panel.getDOMNode());
